979a82c97476a56f3677455c16bf4a12b4355cc1
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2026-07-19 11:05:31 -0500

    long version fixed to accept values

--- src/odoo_project_manager/app.py
+++ src/odoo_project_manager/app.py
@@ -7,7 +7,7 @@ from src.odoo_project_manager.manager import Manager
 def cli():
     args = sys.argv
     options = "hs:l:e:n:"
-    long_versions = ["help", "output-lcoation", "edition", "project-source", "name"]
+    long_versions = ["help", "output-lcoation=", "edition=", "project-source=", "name="]
     arg_options, arg_vals = getopt.gnu_getopt(args[1:], options, long_versions)
     import pdb
 

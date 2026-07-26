9e8ed0743fb2913f9efa66c44fb69c0a25d1aca5
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2026-07-26 01:17:21 -0500

    strategy updated to install venv requirement from different ecript and pycharm idea directory generation

--- src/odoo_project_manager/strategy/pycharm.py
+++ src/odoo_project_manager/strategy/pycharm.py
@@ -1,3 +1,7 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+
+
 from src.odoo_project_manager.options import Options
 from src.odoo_project_manager.strategy.strategy import Strategy
 
@@ -6,6 +10,68 @@ class PycharmStrategy(Strategy):
 
     def __init__(self, manager, options: Options):
         super().__init__(manager, options)
+
+    def run_create(self):
+        super().run_create()
+        self.generate_idea_things()
+
+    def generate_idea_things(self):
+        idea_template_path = os.path.join(
+            self.root_directory, "sample", "pycharm", "pycharm_project_template"
+        )
+        self._generate_from_template(idea_template_path, self.project_path)
+
+    def _generate_from_template(self, src_dir_path, dest_dir_path):
+        env = Environment(loader=FileSystemLoader(src_dir_path))
+        idea_module_path = os.path.join(self.project_path, ".idea")
+        relative_odoo_community_path = os.path.relpath(idea_module_path, self.odoo_path)
+        # Todo: change this
+        relative_conf_path = os.path.relpath(idea_module_path, self.odoo_path)
+        context_vars = {
+            "relative_conf_path": relative_conf_path,
+            "relative_odoo_community_path": relative_odoo_community_path,
+            "relative_odoo_enterprise_path": relative_odoo_community_path,
+            "jdk_name": f"Python ({self.options.project_name})",
+            "project_name": self.options.project_name,
+        }
+        breakpoint()
+        for root, dirs, files in os.walk(src_dir_path):
+            for file in files:
+                # Construct the full absolute or relative path to the source file
+                src_path = os.path.join(root, file)
+
+                rel_path = os.path.relpath(src_path, src_dir_path)
+
+                # 2. Split the path to isolate the file name from its folders
+                dir_name, file_name = os.path.split(rel_path)
+
+                if "project_module" in file_name:
+                    file_name = file_name.replace(
+                        "project_module", self.options.project_name
+                    )
+
+                rel_dest_path = os.path.join(dir_name, file_name)
+
+                dest_path = os.path.join(dest_dir_path, rel_dest_path)
+
+                dest_dir_path_now = os.path.dirname(dest_path)
+                os.makedirs(dest_dir_path_now, exist_ok=True)
+
+                # Check if the file is a Jinja template
+                if src_path.endswith(".jinja"):
+                    template_name = rel_path.replace(os.sep, "/")
+                    template = env.get_template(template_name)
+                    rendered_content = template.render(context_vars)
+
+                    # Remove the '.j2' extension from the final file name
+                    final_dest_path = os.path.splitext(dest_path)[0]
+
+                    # Write the rendered content to the new file
+                    with open(final_dest_path, "w", encoding="utf-8") as f:
+                        f.write(rendered_content)
+
+                    print(f"Rendered: {final_dest_path}")
+        breakpoint()
 
     def execute(self):
         super().execute()
--- src/odoo_project_manager/strategy/strategy.py
+++ src/odoo_project_manager/strategy/strategy.py
@@ -15,52 +15,92 @@ class Strategy(ABC):
         self.set_root_directory()
         self.set_bin_directory()
         self.set_project_path()
+        self.get_odoo_source_directory()
 
     def set_root_directory(self):
+        """
+        sets program root directory, that help with determining bin directory and sample directory.
+        """
         current_file = os.path.abspath(__file__)
         self.root_directory = os.path.dirname(os.path.dirname(current_file))
 
     def set_project_path(self):
+        """
+        sets the root path of project to be created
+        """
         self.project_path = os.path.join(
             self.options.output_location, self.options.project_name
         )
 
     def set_bin_directory(self):
+        """
+        sets the bin directory path
+        """
         self.bin_directory = os.path.join(self.root_directory, "bin")
 
+    def get_odoo_source_directory(self):
+        """
+        calls the script to determine the odoo path in current machine and sets the odoo path.
+        """
+        get_odoo_source_script = os.path.join(self.bin_directory, "get_odoo_source.sh")
+        resutl = subprocess.run(
+            [get_odoo_source_script, self.options.version],
+            text=True,
+            capture_output=True,
+        )
+        self.odoo_path = os.path.join("" + resutl.stdout.strip())
+
+    def run_create(self):
+        """
+        handles project creattion and coresponds to create project command.
+        """
+        self.create_directory()
+        self.pull_source()
+        self.create_virtual_env()
+        self.install_requirements()
+        self.copy_or_generate_configuration_file()
+
     def create_directory(self):
+        """
+        creates a project directory.
+        """
         try:
             os.mkdir(self.project_path)
         except Exception as error:
             _logging.warning(error)
 
-    def run_create(self):
-        self.create_directory()
-        self.pull_source()
-        self.create_virtual_env()
-        self.install_requirements()
-        self.copy_configuration_file()
-
     def pull_source(self):
+        """
+        pull the source code of project in a sub project directory.
+        """
+        to_clone_path = os.path.join(
+            self.project_path, self.options.project_name.upper()
+        )
         git_pull_script = os.path.join(self.bin_directory, "git_pull.sh")
         subprocess.call(
             [
                 git_pull_script,
                 self.options.source_location,
-                self.project_path,
+                to_clone_path,
             ]
         )
 
     def create_virtual_env(self):
+        """
+        create a virual env in the project root directory
+        """
         script_path = os.path.join(self.bin_directory, "create_virtual_env.sh")
         subprocess.call([script_path, self.project_path])
 
     def install_requirements(self):
-        script_path = os.path.join(self.bin_directory, "get_odoo_source.sh")
+        """
+        installs the required odoo packages in the virtual env
+        """
+        script_path = os.path.join(self.bin_directory, "install_odoo_requirement.sh")
         venv_path = os.path.join(self.project_path, ".venv", "bin")
-        subprocess.call([script_path, self.options.version, venv_path])
+        subprocess.call([script_path, self.odoo_path, venv_path])
 
-    def copy_configuration_file(self):
+    def copy_or_generate_configuration_file(self):
         pass
 
     def execute(self):
